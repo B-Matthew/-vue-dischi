65ccfc3da9dac8641544895d033f8fbe90d33f5a
Matthew <matsgeco92@hotmail.it>
2021-04-16 19:35:15 +0200

    Esercizio finito + bonus 1

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Attraverso una chiamata ajax all'API di boolean
+// https://flynn.boolean.careers/exercises/api/array/music
+// avremo a disposizione una decina di dischi musicali.
+// Utilizzando vue, stampiamo a schermo una card per ogni album.
+// BONUS: Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+// BONUS 2: Ordinare i dischi per anno di uscita.
+
+function raccoltaMusicale () {
+  new Vue ({
+    el:'.container',
+    data:{
+      items:[],
+      genres:[],
+      selected: "",
+    },
+    methods: {
+      getGenre: function (){
+        for (var i = 0; i < this.items.length; i++) {
+          const item = this.items[i];
+          const genre = item.genre;
+          if (!this.genres.includes(genre)) {
+            this.genres.push(genre);
+          }
+        }
+      },
+
+      filteredGenre: function() {
+        var test = this.items.filter( type =>  type.genre.includes(this.selected));
+        return test;
+      }
+    },
+
+    mounted() {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then(data => {
+        this.items = data.data.response;
+        this.getGenre();
+      })
+      .catch(() => console.log('error'));
+    }
+  });
+}
+
+function init() {
+  raccoltaMusicale();
+}
+
+$(document).ready(init);
